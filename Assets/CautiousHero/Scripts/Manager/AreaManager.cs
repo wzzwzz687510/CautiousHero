2545f5aebe7028f18d7e5d04ecbb37d82967318e
ing w <wzzwzz163@outlook.com>
2020-01-26 23:28:14 +0800

    Fix visual bug in battle

--- Assets/CautiousHero/Scripts/Manager/AreaManager.cs
+++ Assets/CautiousHero/Scripts/Manager/AreaManager.cs
@@ -169,10 +169,12 @@ namespace Wing.RPGSystem
                     if (AlertZone[spawnLoc].Contains(step)) {                        
                         character.MoveToLocation(step, false, false);
                         foreach (var hash in RemainedCreatures[spawnLoc]) {
-                            Entity entity = hash.GetEntity();
-                            int distance = character.Loc.Distance(entity.Loc);
-                            entity.SetVisual(distance <= playerViewDistance);
+                            //Entity entity = hash.GetEntity();
+                            //int distance = character.Loc.Distance(entity.Loc);
+                            //entity.SetVisual(distance <= playerViewDistance);
+                            hash.GetEntity().SetVisual(true);                            
                         }
+                        GridManager.Instance.DiscoverTiles(spawnLoc);
                         BattleManager.Instance.NewBattle(RemainedCreatures[spawnLoc]);
                         RemainedCreatures.Remove(spawnLoc);
                         InBatlleCreatureSets.Add(spawnLoc);
